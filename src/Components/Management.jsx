0c7f55069bf3b3489a0d1abccc2092cdf6ff18d0
DylanSolarteR <dasolarter@udistrital.edu.co>
2024-03-02 21:32:23 -0500

    Seccion parametros y lista procesos

--- src/Components/Management.jsx
+++ src/Components/Management.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import PartitionConfig from "./PartitionsConfig";
+import ExecutedProcess from "./ExecutedProcess";
 
 function Management(){
 
@@ -10,10 +11,10 @@ function Management(){
     };
 
     return(
-        <div id='parameters' className='h-full w-1/4 flex flex-col align-top gap-4 border-4 border-white'>
+        <div id='parameters' className='h-full w-1/4 flex flex-col align-top gap-4 border-4 border-white overflow-auto'>
             <div id="config" className="w-full h-auto flex flex-col items-center justify-around align-top">
-                <div id="wrapper-tipo-mem" className="w-full h-auto flex flex-col items-center justify-around align-top p-2 pb-4">
-                    <h2 className="max-w-full w-[90%] text-white text-xl mb-2">Memory Management</h2>
+                <section id="wrapper-tipo-mem" className="w-full h-auto flex flex-col items-center justify-around align-top p-2 pb-4">
+                    <h2 className="max-w-full w-[90%] font-semibold text-white text-xl mb-2">Memory Management</h2>
                     <select name="tipo-mem" value={memTypeSelected} onChange={handleSelectChange} 
                         className='max-w-full w-[90%] rounded text-emerald-950 text-center h-8'  
                         id="select-mem-type">
@@ -23,14 +24,33 @@ function Management(){
                         <option value="Dinamic_Memory_without_Compaction">Dinamic Memory without Compaction</option>
                         <option value="Dinamic_Memory_with_Compaction">Dinamic Memory with Compaction</option>
                     </select>
-                </div>
-                <div className="w-full h-60 p-2 border-y-4 border-white text-white flex flex-col gap-4 items-center justify-between overflow-x-hidden overflow-y-auto scroll">
+                </section>
+                <section className="w-full h-auto p-2 border-y-4 border-white text-white">
                     <PartitionConfig memType = {memTypeSelected}/>
+                </section>
+            </div>
+            <section id="executed-processes" className="w-full h-full flex flex-col items-center justify-top px-2">
+                <h2 className="max-w-full w-[90%] font-semibold text-white text-xl mb-2">Processes In Execution</h2>
+                <div className="max-w-[90%] h-5/6 max-h-[90%] w-[90%] rounded outline outline-2 outline-white overflow-auto scroll-smooth snap-y">
+                    <table className="w-full max-w-full border-collapse">
+                        <thead>
+                            <tr>
+                                <th className="max-w-[15%] w-[15%] border border-white text-center font-semibold text-white bg-teal-950 sticky top-0 z-10">P. ID</th>
+                                <th className="max-w-[30%] w-[30%] border border-white text-center font-semibold text-white bg-teal-950 sticky top-0 z-10">Task</th>
+                                <th className="max-w-[30%] w-[30%] border border-white text-center font-semibold text-white bg-teal-950 sticky top-0 z-10">Size</th>
+                                <th className="max-w-[25%] w-[25%] border border-white text-center font-semibold text-white bg-teal-950 sticky top-0 z-10">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <ExecutedProcess pid="PC1" name="Bloc de Notas" size="1.048.576"/>
+                        </tbody>
+                    </table>
                 </div>
-            </div>
-            <div id="executed-processes" className="w-full h-auto flex flex-col items-center justify-around p-2">
-                <h2 className="max-w-full w-[90%] text-white text-xl mb-2">Processes In Execution</h2>
-            </div>
+                <div id="program-buttons" className="w-full max-w-full flex flex-row justify-center p-4 gap-2 lg:gap-16 flex-wrap text-white">
+                    {/* <input type="button" value="Unassign" className='max-w-22 w-24 border-2 border-white p-2 rounded-md hover:bg-emerald-800 hover:scale-105'/> */}
+                    <input type="button" value="Clear" className='max-w-22 w-24 border-2 border-white p-2 rounded-md hover:bg-emerald-800 hover:scale-105'/>
+                </div>
+            </section>
                 
 
 
--- src/Components/PartitionsConfig.jsx
+++ src/Components/PartitionsConfig.jsx
@@ -1,49 +1,94 @@
+import TablePartition from "./TablePartition";
+
 function PartitionConfig({memType}){
     
     const renderComponent = () => {
         switch (memType) {
           case 'Fixed_Memory':
             return(
-                <div className="flex flex-row gap-2 justify-center items-center w-full flex-wrap">
-                    <label htmlFor="size_partitions" className="">Partitions Size (kB)</label><input type="text" placeholder="1-15000" inputmode="numeric" pattern="[0-9]*" className="max-w-full text-black text-center"/>
+              <div className="flex flex-col justify-between items-center overflow-x-hidden overflow-y-auto max-h-full h-auto">
+                <div className="py-2 flex flex-row gap-2 justify-center items-center w-full flex-wrap">
+                    <label htmlFor="size_partitions" className="text-lg">Partitions Size (kiB)</label><input type="text" placeholder="1-15360" inputmode="numeric" pattern="[0-9]*" className="max-w-full rounded text-black text-center"/>
                 </div>
+                <div className="py-2 flex flex-row gap-2 justify-center items-center w-full flex-wrap">
+                  <input type="button" value="Save" className='max-w-22 w-24 border-2 border-white p-2 rounded-md hover:bg-emerald-800 hover:scale-105'/>
+                </div>
+                
+              </div>
             )
           case 'Variable_Memory':
             return(
-            <div className="flex flex-row gap-2 justify-center items-center w-full">
-                <p className="text-white">variable</p>
-            <select name="tipo-mem" className='max-w-72 w-full rounded text-emerald-950 text-center h-8'  id="select-tipo-mem">
-                <option value="" disabled selected hidden>Seleccione un algoritmo</option>
-                <option value="first">First Fit</option>
-                <option value="best">Best Fit</option>
-                <option value="worst">Worst Fit</option>
-            </select>
-            </div>
-            )
-          case 'Dinamic_Memory_without_Compaction':
-            return(
-            <div className="flex flex-row gap-2 justify-center items-center w-full">
-                <p className="text-white">dinamico sin compact</p>
-                <select name="tipo-mem" className='max-w-72 w-full rounded text-emerald-950 text-center h-8'  id="select-tipo-mem">
-                    <option value="" disabled selected hidden>Seleccione un algoritmo</option>
+              <div className="flex flex-col justify-between items-center overflow-x-hidden overflow-y-auto h-auto">
+                <div className="py-2 h-5/6 flex flex-row gap-2 justify-around max-w-[90%] w-full overflow-auto">
+                  <div className="py-2 flex flex-col items-center justify-center">
+                    <label htmlFor="number_partitions" className="text-lg text-center">Number<br/>Partitions</label><input type="text" placeholder="1-45" inputmode="numeric" pattern="[0-9]*" className="max-w-16 rounded text-black text-center"/>
+                  </div>
+                  <div className="py-2 flex flex-col items-center justify-center">
+                    <label htmlFor="size_partitions" className="text-lg text-center">Partitions<br/>Size (kiB)</label><input type="text" placeholder="1-15360" inputmode="numeric" pattern="[0-9]*" className="max-w-20 rounded text-black text-center"/>
+                  </div>
+                  <div className="py-2 flex flex-col items-center justify-center">
+                    <input type="button" value="Add" className='max-w-22 w-24 border-2 border-white p-2 rounded-md hover:bg-emerald-800 hover:scale-105'/>
+                  </div>
+                </div>
+
+                <div className="my-2 max-w-[90%] h-full max-h-[83.333333%] w-[90%] rounded outline outline-2 outline-white overflow-auto scroll-smooth snap-y"> {/*tabla de particiones a añadir*/}
+                  <table className="w-full max-w-full border-collapse">
+                    <thead>
+                      <tr>
+                          <th className="max-w-[15%] w-full border border-white text-center font-semibold text-white bg-teal-950 sticky top-0 z-10">#</th>
+                          <th className="max-w-[60%] w-full border border-white text-center font-semibold text-white bg-teal-950 sticky top-0 z-10">Size</th>
+                          <th className="max-w-[25%] w-full border border-white text-center font-semibold text-white bg-teal-950 sticky top-0 z-10">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <TablePartition number="1" size="2097152"/>
+                    </tbody>
+                  </table>
+                </div>
+
+                <div className="py-2 flex flex-row justify-center items-center w-full">
+                  <select name="tipo-mem" className='max-w-72 w-full rounded text-emerald-950 text-center h-8'  id="select-tipo-mem">
+                    <option value="" disabled selected hidden>Select the allocation algorithm</option>
                     <option value="first">First Fit</option>
                     <option value="best">Best Fit</option>
                     <option value="worst">Worst Fit</option>
-                </select>
-            </div>
+                  </select>
+                </div>
+
+                <div className="py-2 flex flex-row gap-2 justify-center items-center w-full flex-wrap">
+                  <input type="button" value="Save" className='max-w-22 w-24 border-2 border-white p-2 rounded-md hover:bg-emerald-800 hover:scale-105'/>
+                </div>
+              </div>
+            )
+          case 'Dinamic_Memory_without_Compaction':
+            return(
+              <div className="flex flex-col justify-between items-center overflow-x-hidden overflow-y-auto max-h-full h-auto">
+                <div className="py-2 flex flex-row gap-2 justify-center items-center w-full">
+                  <select name="tipo-mem" className='max-w-72 w-full rounded text-emerald-950 text-center h-8'  id="select-tipo-mem">
+                    <option value="" disabled selected hidden>Select the allocation algorithm</option>
+                    <option value="first">First Fit</option>
+                    <option value="best">Best Fit</option>
+                    <option value="worst">Worst Fit</option>
+                  </select>
+                </div>
+                <div className="py-2 flex flex-row gap-2 justify-center items-center w-full flex-wrap">
+                  <input type="button" value="Save" className='max-w-22 w-24 border-2 border-white p-2 rounded-md hover:bg-emerald-800 hover:scale-105'/>
+                </div>
+              </div>
             )
           case 'Dinamic_Memory_with_Compaction':
             return(
-            <div className="flex flex-row gap-2 justify-center items-center w-full">
-                <p className="text-white">dinamico con compact</p>
-            </div>
+              <div className="flex flex-col justify-center items-center overflow-x-hidden overflow-y-auto max-h-[13.5rem] h-auto">
+                <div className="py-2 flex flex-row gap-2 justify-center items-center w-full flex-wrap">
+                  <input type="button" value="Compact" className='max-w-22 w-24 border-2 border-white p-2 rounded-md hover:bg-emerald-800 hover:scale-105'/>
+                </div>
+              </div>
             )
           default:
-            return(<>
-                <div className="flex flex-row gap-2 justify-center items-center w-full">
-                    
+            return(
+                <div className="flex flex-col items-center gap-2 w-full text-center justify-center min-h-52">
+                    <p className="font-medium text-3xl">Choose a memory type to configure the parameters</p>
                 </div>
-            </>
                 
             )
         }
